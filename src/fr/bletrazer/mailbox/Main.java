a96f9a0bd936af71418bb8e2dea25677d733e7b4
Mickael <bletrazer@gmail.com>
2020-05-01 10:57:31 +0400

    Chargement plugin
    Le plugin ne se charge plus si il n'a pas pu se connecter a la base de donnée

--- src/fr/bletrazer/mailbox/Main.java
+++ src/fr/bletrazer/mailbox/Main.java
@@ -46,6 +46,7 @@ public class Main extends JavaPlugin {
 			
 		} else {
 			this.getLogger().log(Level.SEVERE, LangManager.getValue("connection_needed"));
+			this.getServer().getPluginManager().disablePlugin(this);
 		}
 
 	}
